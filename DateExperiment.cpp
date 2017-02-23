243da7f3dd7d156b44cccdbd8f050b3eb1a46352
Kenneth Adair <SOREASAN@GMAIL.COM>
2017-02-23 11:01:15 -0700

    Created an experiment with C style dates.

---
+++ DateExperiment.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <time.h>
+using namespace std;
+
+int main(){
+    //string goalstring = "Date: Thu, 19 Feb 2009 12:27:04 GMT\n";
+    
+    time_t rawtime;
+    struct tm * timeinfo;
+    char buffer[80];
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    strftime(buffer, 80, "Now it's %I:%M%p.", timeinfo);
+    puts(buffer);
+    
+    strftime(buffer, 80, "Date: %c %Z", timeinfo);
+    puts(buffer);
+
+    string test = "";
+    test += buffer;
+    cout << test << endl;
+    return 0;
+}
